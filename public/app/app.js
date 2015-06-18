204027c60454872359049b34c881d70db8838112
Abhimanyu Babbar <babbar@kth.se>
2015-06-18 14:53:41 +0200

    Added chart example.

--- public/app/app.js
+++ public/app/app.js
@@ -1,26 +1,26 @@
 'use strict';
-(function(){
-	console.log('going to register the module');
-	var app = angular.module('stats',['ngRoute']);
+(function() {
+    console.log('going to register the module');
+    var app = angular.module('stats', ['ngRoute']);
 
-	app.config(['$logProvider', '$routeProvider', function($logProvider, $routeProvider){
-		$logProvider.debugEnabled(true);
+    app.config(['$logProvider', '$routeProvider',
+        function($logProvider, $routeProvider) {
+            $logProvider.debugEnabled(true);
 
-		$routeProvider
-			.when("/", {
+            $routeProvider
+                .when("/", {
 
-				templateUrl:'/views/main.html',
-				controller:'MainController',
-				controllerAs:'mainController'
+                    templateUrl: '/views/main.html',
+                    controller: 'MainController',
+                    controllerAs: 'mainController'
 
-			})
-			.when('/charts',{
+                })
+                .when('/charts', {
 
-				templateUrl:'/views/charts.html',
-				controller: 'MainController',
-				controllerAs: 'chartController'
-			})
-
-	}]);
-
+                    templateUrl: '/views/charts.html',
+                    controller: 'MainController',
+                    controllerAs: 'chartController'
+                })
+        }
+    ])
 })();
